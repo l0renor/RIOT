148678cd8127e2760953f6708e823fa436448399
Koen Zandberg <koen@bergzand.net>
2017-09-22 11:32:03 +0200

    ethos: Allow dd IPv6 address to tap interface

--- dist/tools/ethos/start_network.sh
+++ dist/tools/ethos/start_network.sh
@@ -10,6 +10,10 @@ create_tap() {
     ip a a fe80::1/64 dev ${TAP}
     ip a a fd00:dead:beef::1/128 dev lo
     ip route add ${PREFIX} via fe80::2 dev ${TAP}
+    if [ ! -z ${TAP_IP} ] 
+    then
+        ip a a ${TAP_IP} dev ${TAP}
+    fi
 }
 
 remove_tap() {
@@ -36,12 +40,15 @@ BAUDRATE=115200
 UHCPD="$(readlink -f "${ETHOS_DIR}/../uhcpd/bin")/uhcpd"
 
 [ -z "${PORT}" -o -z "${TAP}" -o -z "${PREFIX}" ] && {
-    echo "usage: $0 <serial-port> <tap-device> <prefix> [baudrate]"
+    echo "usage: $0 <serial-port> <tap-device> <prefix> [baudrate] [tap-addr]"
     exit 1
 }
 
 [ ! -z $4 ] && {
     BAUDRATE=$4
+    [ ! -z $5 ] && {
+        TAP_IP=$5
+    }
 }
 
 trap "cleanup" INT QUIT TERM EXIT
