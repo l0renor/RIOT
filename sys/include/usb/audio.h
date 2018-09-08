dfb29be6a748e02cea7135d5a9945f739c122713
Koen Zandberg <koen@bergzand.net>
2018-09-08 10:19:13 +0200

    usb: audio descriptors

--- sys/include/usb/audio.h
+++ sys/include/usb/audio.h
@@ -36,18 +36,7 @@ typedef struct __attribute__((packed)) {
     uint8_t category;
     uint16_t totallength;
     uint8_t controls;
-} usb_descriptor_audio_interface_t;
-
-typedef struct __attribute__((packed)) {
-    uint8_t length;             /**< Size of this descriptor */
-    uint8_t type;               /**< Descriptor type (@ref USB_TYPE_DESCRIPTOR_DEVICE)  */
-    uint8_t subtype;            /**< Descriptor subtype                                 */
-    uint8_t clockid;
-    uint8_t attributes;
-    uint8_t controls;
-    uint8_t assocterminal;
-    uint8_t clocksource;
-} usb_descriptor_audio_clock_t;
+} usb_descriptor_ac_interface_t;
 
 typedef struct __attribute__((packed)) {
     uint8_t length;             /**< Size of this descriptor */
@@ -55,6 +44,7 @@ typedef struct __attribute__((packed)) {
     uint8_t subtype;            /**< Descriptor subtype                                 */
     uint8_t clockid;
     uint8_t nrinpins;
+} usb_descriptor_ac_clock
 
 typedef struct __attribute__((packed)) {
     uint8_t length;             /**< Size of this descriptor */
@@ -64,12 +54,12 @@ typedef struct __attribute__((packed)) {
     uint16_t terminaltype;
     uint8_t assocterminal;
     uint8_t clocksourceid;
-    uint8_t nrchannels;
-    uint32_t channelconfig;
-    uint8_t channelnamesidx;
-    uint16_t controls;
-    uint8_t terminalidx;
-} usb_descriptor_audio_input_terminal_t;
+    uint32_t controls;
+    uint16_t clusterdescridx;
+    uint16_t terminaldescridx;
+    uint16_t connectordercridx;;
+    uint16_t terminalidx;
+} usb_descriptor_ac_input_terminal_t;
 
 typedef struct __attribute__((packed)) {
     uint8_t length;             /**< Size of this descriptor */
@@ -80,11 +70,60 @@ typedef struct __attribute__((packed)) {
     uint8_t assocterminal;
     uint8_t sourceid;
     uint8_t clocksourceid;
-    uint16_t controls;
-    uint8_t terminalidx;
-} usb_descriptor_audio_output_terminal_t;
+    uint32_t controls;
+    uint16_t terminaldescridx;
+    uint16_t connectordercridx;;
+    uint16_t terminalidx;
+} usb_descriptor_ac_output_terminal_t;
 
+typedef struct __attribute__((packed)) {
+    uint8_t length;             /**< Size of this descriptor */
+    uint8_t type;               /**< Descriptor type (@ref USB_TYPE_DESCRIPTOR_DEVICE)  */
+    uint8_t subtype;            /**< Descriptor subtype                                 */
+    uint8_t unitid;
+    uint8_t nrpins;
+} usb_descriptor_ac_mixer_unit_t;
 
+typedef struct __attribute__((packed)) {
+    uint8_t length;             /**< Size of this descriptor */
+    uint8_t type;               /**< Descriptor type (@ref USB_TYPE_DESCRIPTOR_DEVICE)  */
+    uint8_t subtype;            /**< Descriptor subtype                                 */
+    uint8_t clockid;
+    uint8_t attributes;
+    uint8_t controls;
+    uint8_t referenceterminal;
+    uint8_t clocksourceidx;
+} usb_descriptor_ac_clock_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;             /**< Size of this descriptor */
+    uint8_t type;               /**< Descriptor type (@ref USB_TYPE_DESCRIPTOR_DEVICE)  */
+    uint8_t subtype;            /**< Descriptor subtype                                 */
+    uint8_t clockid;
+    uint8_t clocksourceid;
+    uint8_t controls;
+    uint8_t multiplieridx;
+} usb_decriptor_ac_clock_mult_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;             /**< Size of this descriptor */
+    uint8_t type;               /**< Descriptor type (@ref USB_TYPE_DESCRIPTOR_DEVICE)  */
+    uint8_t subtype;            /**< Descriptor subtype                                 */
+    uint8_t pwrdomainid;
+    uint16_t recoverytime1;     /**< Recovery time from D1 to D0 in 50us increments     */
+    uint16_t recoverytime2;     /**< Recovery time from D2 to D0 in 50us increments     */
+    uint8_t nrentities;
+} usb_descriptor_ac_power_domain_t;
+
+typedef struct __attribute__((packed)) {
+    uint8_t length;             /**< Size of this descriptor */
+    uint8_t type;               /**< Descriptor type (@ref USB_TYPE_DESCRIPTOR_DEVICE)  */
+    uint8_t subtype;            /**< Descriptor subtype                                 */
+    uint16_t bcdadc;            /**< Audio class specification release                  */
+    uint8_t category;
+    uint16_t totallength;
+    uint8_t controls;
+} usb_descriptor_as_interface_t;
 
 #ifdef __cplusplus
 }
