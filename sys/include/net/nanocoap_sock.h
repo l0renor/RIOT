6ffc72560b437fbd1bbfede097924750cc9edb82
Kaspar Schleiser <kaspar@schleiser.de>
2019-03-18 11:23:57 +0100

    fixup! nanocoap_sock: WIP nanocoap_get_blockwise()

--- sys/include/net/nanocoap_sock.h
+++ sys/include/net/nanocoap_sock.h
@@ -25,6 +25,7 @@
 
 #include "net/nanocoap.h"
 #include "net/sock/udp.h"
+#include "net/sock/util.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -73,7 +74,11 @@ ssize_t nanocoap_get_blockwise(sock_udp_ep_t *remote, const char *path,
                                coap_blksize_t blksize,
                                coap_blockwise_cb_t callback, void *arg);
 
+int nanocoap_get_blockwise_url(const char *url, coap_blksize_t blksize,
+                               coap_blockwise_cb_t callback, void *arg);
 
+int nanocoap_get_blockwise_url_buf(const char *url, coap_blksize_t blksize,
+                                   uint8_t *buf, size_t len);
 /**
  * @brief   Simple synchronous CoAP request
  *
