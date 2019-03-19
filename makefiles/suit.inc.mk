63fdb6a9600325b458cf6c128f0e1115dff01386
Kaspar Schleiser <kaspar@schleiser.de>
2019-03-19 16:00:45 +0100

    makefiles/suit: add basic node update triggering

--- makefiles/suit.inc.mk
+++ makefiles/suit.inc.mk
@@ -53,5 +53,17 @@ suit/publish: $(SUIT_MANIFESTS) $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
 		echo "       as \"$(SUIT_COAP_ROOT)/$$(basename $$file)\""; \
 	done
 
+suit/notify:
+	@test -n "$(SUIT_CLIENT)"
+	$(Q)TARGET_SLOT=$$(aiocoap-client "coap://$(SUIT_CLIENT)/suit/slot/inactive") && \
+	if [ "$$TARGET_SLOT" = 0 ]; then \
+		export PAYLOAD="$(SUIT_COAP_ROOT)/$$(basename $(SLOT0_SUIT_MANIFEST))"; \
+	else \
+		export PAYLOAD="$(SUIT_COAP_ROOT)/$$(basename $(SLOT1_SUIT_MANIFEST))"; \
+	fi && \
+	aiocoap-client -m POST "coap://$(SUIT_CLIENT)/suit/trigger" \
+		--payload "$${PAYLOAD}" && \
+		echo "Triggered $(SUIT_CLIENT) to update slot $$TARGET_SLOT"
+
 suit/genkey:
 	$(RIOTBASE)/dist/tools/suit_v1/gen_key.py
